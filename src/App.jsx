db754244d09be086fd62e645663dc1f7bc5ffc1c
Nicolas Lukestik <nicolaslukestik@gmail.com>
2021-09-02 18:59:53 -0300

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles, useTheme} from '@material-ui/styles'
+import { makeStyles } from '@material-ui/styles'
 import { Grid } from '@material-ui/core'
 import NavBar from './components/NavBar.jsx'
 import BackToTop from './components/BackToTop.jsx'
